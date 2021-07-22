0cc454ec03db8465e2f0b1a37bb0f171728c6269
Mila Eldora <meldora@student.21-school.ru>
2021-07-22 17:04:02 +0300

    correct file descriptor management in heredoc exec

--- heredoc_exec_bonus.c
+++ heredoc_exec_bonus.c
@@ -24,6 +24,7 @@ static void	execute_heredoc(int cmd1_in, char *delim)
 		else
 			write_to_pipe(cmd1_in, &string);
 	}
+	close(cmd1_in);
 }
 
 static void	execute_cmd(int in_fd, int out_fd, char *cmd, char **envp)
@@ -42,17 +43,10 @@ static void	execute_cmd(int in_fd, int out_fd, char *cmd, char **envp)
 		execve(args[0], args, envp);
 		exit(127);
 	}
+	close(in_fd);
+	close(out_fd);
 	free_string_arr(args);
 	append_pid(get_pid_list(), pid);
-}
-
-static void	close_all_fds(int cmd1_fd[2], int cmd2_fd[2], int file_fd)
-{
-	close(cmd1_fd[0]);
-	close(cmd1_fd[1]);
-	close(cmd2_fd[0]);
-	close(cmd2_fd[1]);
-	close(file_fd);
 }
 
 int	heredoc_exec(char **av, char **envp)
@@ -60,7 +54,6 @@ int	heredoc_exec(char **av, char **envp)
 	int		file_fd;
 	int		cmd1_fd[2];
 	int		cmd2_fd[2];
-	pid_t	pid;
 
 	file_fd = open(av[5], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (file_fd == -1)
@@ -70,14 +63,8 @@ int	heredoc_exec(char **av, char **envp)
 	execute_heredoc(cmd1_fd[1], av[2]);
 	if (handle_cmds(av + 3, 2, envp) != 0)
 		return (set_exit_code(ENOENT));
-	pid = fork();
-	if (pid == 0)
-	{
-		execute_cmd(cmd1_fd[0], cmd2_fd[1], av[3], envp);
-		execute_cmd(cmd2_fd[0], file_fd, av[4], envp);
-		exit(0);
-	}
-	close_all_fds(cmd1_fd, cmd2_fd, file_fd);
+	execute_cmd(cmd1_fd[0], cmd2_fd[1], av[3], envp);
+	execute_cmd(cmd2_fd[0], file_fd, av[4], envp);
 	wait_for_all_procs();
 	free_pid_list();
 	return (get_exit_code());
